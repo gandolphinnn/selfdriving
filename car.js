131ea2935f2124ea9cc09f9ca1edabb80708eede
gandnolphinnn <gandolfiluca03@gmail.com>
2022-03-31 13:57:36 +0200

    circuit and new rays and controls

--- car.js
+++ car.js
@@ -42,6 +42,9 @@ class Ray {
 			}
 		});
 		this.length = Math.sqrt((this.p.x - this.endP.x) ** 2 + (this.p.y - this.endP.y) ** 2);
+		if (this.length <= 2) {
+			gameover = true;
+		}
 	}
 }
 class Car {
@@ -51,20 +54,32 @@ class Car {
 		this.speed = speed; // value must stay between -10 and 100 and then divided for 10
 		this.width = width; // view from the front
 		this.length = length; // view from the side
-		this.points = new Array(10);
-		this.rays = new Array(8);
+		this.points = new Array(16);
+		this.rays = new Array(16);
 		this.calcPointsRays();
 	}
 	userDrive() {
-		switch (move) {
-			case 'gas': break;
-			case 'left': break;
-			case 'right': break;
-			case 'reverse': break;
+		if (move.gas) {
+			this.speed ++;
+		}
+		else if (move.reverse) {
+			this.speed -= 5;
+		}
+		else if (this.speed > 0) {
+			this.speed --;
+		}
+		else if (this.speed < 0) {
+			this.speed++;
+		}
+		if (move.left && Math.abs(this.speed) > 5) {
+			this.degr += 2;
+		}
+		else if (move.right && Math.abs(this.speed) > 5) {
+			this.degr -= 2;
 		}
 	}
 	autoDrive() {
-
+		
 	}
 	updatePos() {
 		if (this.speed > 100)
@@ -79,33 +94,42 @@ class Car {
 	calcPointsRays() {
 		this.points[0] = new Point(this.c.x+Math.cos(formA(this.degr, 'rad'))*this.length/2,this.c.y-Math.sin(formA(this.degr, 'rad'))*this.length/2);
 		let rad = formA(this.degr + 90, 'rad'); 
-		this.points[1] = new Point(this.points[0].x+Math.cos(rad)*this.width/6, this.points[0].y-Math.sin(rad)*this.width/6);
-		this.points[2] = new Point(this.points[0].x-Math.cos(rad)*this.width/6, this.points[0].y+Math.sin(rad)*this.width/6);
-		this.points[3] = new Point(this.points[1].x+Math.cos(rad)*this.width/6, this.points[1].y-Math.sin(rad)*this.width/6);
-		this.points[4] = new Point(this.points[2].x-Math.cos(rad)*this.width/6, this.points[2].y+Math.sin(rad)*this.width/6);
-		this.points[5] = new Point(this.points[3].x+Math.cos(rad)*this.width/6, this.points[3].y-Math.sin(rad)*this.width/6);
-		this.points[6] = new Point(this.points[4].x-Math.cos(rad)*this.width/6, this.points[4].y+Math.sin(rad)*this.width/6);
-		this.points[7] = new Point(this.c.x-Math.cos(formA(this.degr, 'rad'))*this.length/2,this.c.y+Math.sin(formA(this.degr, 'rad'))*this.length/2);
-		this.points[8] = new Point(this.points[7].x+Math.cos(rad)*this.width/2, this.points[7].y-Math.sin(rad)*this.width/2);
-		this.points[9] = new Point(this.points[7].x-Math.cos(rad)*this.width/2, this.points[7].y+Math.sin(rad)*this.width/2);
+		this.points[1] = new Point(this.points[0].x+Math.cos(rad)*this.width/12, this.points[0].y-Math.sin(rad)*this.width/12);
+		this.points[2] = new Point(this.points[0].x-Math.cos(rad)*this.width/12, this.points[0].y+Math.sin(rad)*this.width/12);
+		this.points[3] = new Point(this.points[1].x+Math.cos(rad)*this.width/12, this.points[1].y-Math.sin(rad)*this.width/12);
+		this.points[4] = new Point(this.points[2].x-Math.cos(rad)*this.width/12, this.points[2].y+Math.sin(rad)*this.width/12);
+		this.points[5] = new Point(this.points[3].x+Math.cos(rad)*this.width/12, this.points[3].y-Math.sin(rad)*this.width/12);
+		this.points[6] = new Point(this.points[4].x-Math.cos(rad)*this.width/12, this.points[4].y+Math.sin(rad)*this.width/12);
+		this.points[7] = new Point(this.points[5].x+Math.cos(rad)*this.width/12, this.points[5].y-Math.sin(rad)*this.width/12);
+		this.points[8] = new Point(this.points[6].x-Math.cos(rad)*this.width/12, this.points[6].y+Math.sin(rad)*this.width/12);
+		this.points[9] = new Point(this.points[7].x+Math.cos(rad)*this.width/12, this.points[7].y-Math.sin(rad)*this.width/12);
+		this.points[10]= new Point(this.points[8].x-Math.cos(rad)*this.width/12, this.points[8].y+Math.sin(rad)*this.width/12);
+		this.points[11]= new Point(this.points[9].x+Math.cos(rad)*this.width/12, this.points[9].y-Math.sin(rad)*this.width/12);
+		this.points[12]= new Point(this.points[10].x-Math.cos(rad)*this.width/12, this.points[10].y+Math.sin(rad)*this.width/12);
+		this.points[13]= new Point(this.c.x-Math.cos(formA(this.degr, 'rad'))*this.length/2,this.c.y+Math.sin(formA(this.degr, 'rad'))*this.length/2);
+		this.points[14]= new Point(this.points[13].x+Math.cos(rad)*this.width/2, this.points[13].y-Math.sin(rad)*this.width/2);
+		this.points[15]= new Point(this.points[13].x-Math.cos(rad)*this.width/2, this.points[13].y+Math.sin(rad)*this.width/2);
 		let degrArr = [this.degr, formA(this.degr + 10), formA(this.degr - 10), formA(this.degr + 20),
-			formA(this.degr - 20), formA(this.degr + 35), formA(this.degr - 35), formA(this.degr + 180)];
-		for (let i = 0; i < 8; i++) {
+			formA(this.degr - 20), formA(this.degr + 30), formA(this.degr - 30), formA(this.degr + 40),
+			formA(this.degr - 40), formA(this.degr + 50), formA(this.degr - 50), formA(this.degr + 60),
+			formA(this.degr - 60), formA(this.degr + 180), formA(this.degr + 135), formA(this.degr + 225)];
+		for (let i = 0; i < this.rays.length; i++) {
 			this.rays[i] = new Ray(this.points[i], degrArr[i]);
 		}
 	}
 	draw() {		
 		ctx.beginPath();
-		ctx.moveTo(this.points[5].x, this.points[5].y);
-		ctx.lineTo(this.points[6].x, this.points[6].y);
-		ctx.lineTo(this.points[9].x, this.points[9].y);
-		ctx.lineTo(this.points[8].x, this.points[8].y);
-		ctx.lineTo(this.points[5].x, this.points[5].y);
+		ctx.moveTo(this.points[11].x, this.points[11].y);
+		ctx.lineTo(this.points[12].x, this.points[12].y);
+		ctx.lineTo(this.points[15].x, this.points[15].y);
+		ctx.lineTo(this.points[14].x, this.points[14].y);
+		ctx.lineTo(this.points[11].x, this.points[11].y);
 		ctx.moveTo(this.c.x, this.c.y);
 		ctx.lineTo(this.points[0].x, this.points[0].y);
 		ctx.stroke();
 		this.rays.forEach(ray => {
 			ray.draw(this.length);
 		});
+		ctx.fillText(this.speed, 10, 10)		
 	}
 }
--- main.js
+++ main.js
@@ -14,19 +14,35 @@ window.onload = function() {
 		mouse.pos.x = e.clientX;
 	});
 	document.addEventListener('keydown', function(e) {
-		switch(e.code){
-			case 'KeyW': case 'ArrowUp': // gas pedal
-				break;
-			case 'KeyA': case 'ArrowLeft': // left
-				break;
-			case 'KeyD': case 'ArrowRight': // right
-				break;
-			case 'KeyS': case 'ArrowDown': // break pedal / reverse gear
-				break;
-			case 'Enter': //switch mode
-				mode = false; //! change this in a boolean variable
-				break;
-			default: break;
+		if ((e.code == 'KeyW' || e.code == 'ArrowUp') && !move.gas) {
+			move.gas = true;
+		}
+		if ((e.code == 'KeyA' || e.code == 'ArrowLeft') && !move.left) {
+			move.left = true;
+		}
+		if ((e.code == 'KeyD' || e.code == 'ArrowRight') && !move.right) {
+			move.right = true;
+		}
+		if ((e.code == 'KeyS' || e.code == 'ArrowDown') && !move.reverse) {
+			move.reverse = true;
+		}
+		if (e.code == 'Enter') {
+			autopilot = !autopilot;
+			car.speed = 0;
+		}
+	});
+	document.addEventListener('keyup', function(e) {
+		if ((e.code == 'KeyW' || e.code == 'ArrowUp') && move.gas) {
+			move.gas = false;
+		}
+		if ((e.code == 'KeyA' || e.code == 'ArrowLeft') && move.left) {
+			move.left = false;
+		}
+		if ((e.code == 'KeyD' || e.code == 'ArrowRight') && move.right) {
+			move.right = false;
+		}
+		if ((e.code == 'KeyS' || e.code == 'ArrowDown') && move.reverse) {
+			move.reverse = false;
 		}
 	});
 	let mouse = {
@@ -34,27 +50,41 @@ window.onload = function() {
 		state: 0
 	}
 	function animate() {
-		requestAnimationFrame(animate);
+		if (!gameover) {
+			requestAnimationFrame(animate);
+		}
 		ctx.clearRect(0,0, innerWidth, innerHeight);
-		walls.forEach(wall => { wall.draw() });
-		if (mode == 'auto') { //!
+		walls.forEach(wall => { wall.draw()});
+		if (autopilot) {
 			car.autoDrive();
 		}
-		if (mode == 'user') {
+		if (!autopilot) {
 			car.userDrive();
 		}
-		car.rays.forEach(ray => {
-			ray.calculateEnd();
-		});
+		car.updatePos();
+		car.rays.forEach(ray => {ray.calculateEnd()});
 		car.draw();
 	}
 	
 	ctx.fillStyle = 'orange';
 	ctx.strokeStyle = 'gray';
 	ctx.lineWidth = '3';
-	walls.push(new Wall(new Point(400, 400), new Point(200, 200)));	
-	walls.push(new Wall(new Point(1000, 400), new Point(1000, 200)));	
-	let car = new Car(new Point(500, 300), 0, 0, 30, 50);
-	
+	const ep = [new Point(324, 58), new Point(190, 77), new Point(110, 170), new Point(75, 370),
+			new Point(95, 676), new Point(271, 875), new Point(485, 900), new Point(1419, 894),
+			new Point(1680, 870), new Point(1822, 773), new Point(1874, 595), new Point(1862, 304),
+			new Point(1816, 115), new Point(1720, 69), new Point(1610, 60), new Point(1060, 150),
+			new Point(760, 150), new Point(550, 120)];
+	const ip = [new Point(500, 300), new Point(370, 227), new Point(295, 215), new Point(250, 240),
+			new Point(244, 373), new Point(284, 648), new Point(400, 734), new Point(640, 760),
+			new Point(1400, 750), new Point(1600, 730), new Point(1700, 600), new Point(1700, 360),
+			new Point(1660, 280), new Point(1550, 250), new Point(1111, 317), new Point(824, 317)];
+	for (let i = 0; i < ep.length; i++) {
+		walls.push(new Wall(ep[i], ep[(i + 1) % ep.length]));
+	}
+	for (let i = 0; i < ip.length; i++) {
+		walls.push(new Wall(ip[i], ip[(i + 1) % ip.length]));
+	}
+	let car = new Car(new Point(750, 830), 0, 0, 30, 50);
+	autopilot = false;
 	animate();
 }
